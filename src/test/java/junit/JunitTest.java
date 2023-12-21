f0807b6f1d989023c6945a50d3d160bb5dc438e1
AlexQAEnginer <Lekseu2007@yandex.ru>
2023-12-21 23:41:29 +0500

    add

--- src/test/java/junit/JunitTest.java
+++ src/test/java/junit/JunitTest.java
@@ -1,5 +1,12 @@
 package junit;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class JunitTest {
-@test
+@Test
+    void fistTest(){
+    Assertions.assertTrue(3 > 2);
+}
+
 }
