c4eb566ffe570893f1adccd9a94e64a03e263d63
AlexQAEnginer <Lekseu2007@yandex.ru>
2023-12-23 12:39:16 +0500

    add

--- src/test/java/junit/JunitTest.java
+++ src/test/java/junit/JunitTest.java
@@ -1,26 +1,26 @@
 package junit;
 
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class JunitTest {
 @Test
     void fistTest(){
 
     open("https://demoqa.com/text-box");
+
     $("#userName").setValue("Alexey Kozharin");
     $("#userEmail").setValue("Lekseu7@yandex.ru");
     $("#currentAddress").setValue("Pytevay 18A");
     $("#permanentAddress").setValue("70");
-    $("#submit").click();
 
-    $("#output").shouldBe(visible);
+    sleep(15000);
+
+
 
 }
 
