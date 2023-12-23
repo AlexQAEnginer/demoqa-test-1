5309fca71ccf36de6c134a045a0dc3c1cf8d2103
AlexQAEnginer <Lekseu2007@yandex.ru>
2023-12-23 20:41:21 +0500

    add

--- src/test/java/junit/JunitTest.java
+++ src/test/java/junit/JunitTest.java
@@ -1,27 +1,40 @@
 package junit;
 
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideDriver;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static jdk.dynalink.StandardNamespace.ELEMENT;
 
 public class JunitTest {
+@BeforeAll
+static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadTimeout = 40000;
+        }
+
 @Test
     void fistTest(){
 
-    open("https://demoqa.com/text-box");
-
+    open("/text-box");
     $("#userName").setValue("Alexey Kozharin");
     $("#userEmail").setValue("Lekseu7@yandex.ru");
     $("#currentAddress").setValue("Pytevay 18A");
     $("#permanentAddress").setValue("70");
 
-    sleep(15000);
-
-
-
 }
 
 }
+
