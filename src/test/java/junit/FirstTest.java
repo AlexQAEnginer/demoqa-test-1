797f7333e7d9c7bf263f9e836ce6ddbd6bb899fc
AlexQAEnginer <Lekseu2007@yandex.ru>
2024-01-09 22:09:31 +0500

    add

--- src/test/java/junit/FirstTest.java
+++ src/test/java/junit/FirstTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class FirstTest {
@@ -19,7 +20,10 @@ public class FirstTest {
         $("span.flex-1").click();
         $("#query-builder-test").setValue("Selenide").pressEnter();
         $$("[data-testid=results-list]").first().$("a").click();
-        sleep(50000);
+        $$("ul.UnderlineNav-body li").get(5).$("#wiki-tab").click();
+        $$("ul[data-filterable-type=substring] li").get(17).$("a.Truncate-text").shouldHave(text("Selenide vs Selenium")).click();
+        $("div.markdown-body p").shouldHave(text("This page brings an overview of how Selenide API is simpler and more powerful than"));
+        sleep(5000);
 
     }
 }
