c5933d82a53a37f9d2b6e6787914d0baca5c3c49
AlexQAEnginer <Lekseu2007@yandex.ru>
2024-01-20 17:13:47 +0500

    add

--- src/test/java/junit/FourTest.java
+++ src/test/java/junit/FourTest.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -29,6 +31,11 @@ public class FourTest {
         $(".react-datepicker__year-select").selectOption("1992");
         $(".react-datepicker__month-select").selectOption("July");
         $(".react-datepicker__day--006").click();
+        $("#subjectsContainer").setValue("Test").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/img/test.png"));
+        $("#currentAddress").setValue("information");
+
         sleep(10000);
 
     }
