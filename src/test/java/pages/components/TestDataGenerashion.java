4e5223de8230128fa8f340a4e739bc906fe17cbf
AlexQAEnginer <Lekseu2007@yandex.ru>
2024-03-04 22:10:31 +0500

    add home

--- src/test/java/pages/components/TestDataGenerashion.java
+++ src/test/java/pages/components/TestDataGenerashion.java
@@ -1,6 +1,7 @@
 package pages.components;
 
 import java.security.SecureRandom;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 public class TestDataGenerashion {
@@ -8,8 +9,14 @@ public class TestDataGenerashion {
         System.out.println(randomString(10));
         String userTest = (randomString(10) + ".ru");
         System.out.println(userTest);
-    }
+        System.out.println(randomInt(1,78));
 
+        String[] names = {"a","b","c","d","e"};
+        System.out.println(randomItem(names));
+    }
+    public static int randomInt(int min, int max) {
+        return ThreadLocalRandom.current().nextInt(min,max -1);
+    }
 
     public static String randomString(int len) {
         final String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
@@ -21,5 +28,9 @@ public class TestDataGenerashion {
     }
     public static String randomMail(){
         return randomString(10) + ".ru";
+    }
+    public static String randomItem(String[] values){
+        int index = randomInt(1,values.length);
+        return values[index];
     }
 }
